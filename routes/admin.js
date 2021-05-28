bab6cb97f44c7c746c5d5f7e20c7f9a3c560adae
anwarkvzk <anvarvzk521@gmail.com>
2021-05-28 09:07:08 +0530

    Admin product View Table Added

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,33 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
-  res.render('index',{admin:true})
+  let products=[
+    {
+      name:"I Phone 12",
+      category:"Mobile",
+      description:"This Phone made by Apple Company",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5D1y9HttB8BUiyr1sOxkxIPNklSelv51ybHVzhY8UgJgPgZvHNTXLaN55BXy2TcsJ0DGD7Dr5&usqp=CAc"
+    },
+    {
+      name:"Samsung Galaxy A30s",
+      category:"Mobile",
+      description:"This Phone is very budget phone",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_yisu1OsohJnGoViGm2J01VFl0A_hpsRf8g&usqp=CAU"
+    },
+    {
+      name:"One Plus 8",
+      category:"Mobile",
+      description:"This Phone is very good  smartphone",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSayTK0z1I2VqJnmSktdbX2xZxt_wIOLUztPQ&usqp=CAU"
+    },
+   {
+     name:"Alexa",
+     category:"Device",
+     description:"This is a smart device",
+     image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSft1Iez8vRv9H4FfHEkimX-g0vao1o285JFg&usqp=CAU"
+   }
+  ]
+  res.render('admin/view-products',{admin:true,products})
 });
 
 module.exports = router;
